c6c4336eb03a1a36f4a785bd08a03ccd0809f6e0
haochend413 <dinghaochen413@gmail.com>
2025-01-14 19:50:02 -0600

    feat: added Ubuntu font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,14 @@ import type { Metadata } from "next";
 import Head from "./_components/Head";
 import "./global.css";
 
+// pages/_app.js
+import { Ubuntu } from "next/font/google";
+
+// Use the font as a className in your components
+const ubuntu = Ubuntu({
+  weight: ["300", "700"],
+});
+
 //I suppose here it handles discription and title
 export const metadata: Metadata = {
   title: "Hello, visitors!",
@@ -15,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={ubuntu.className}>
       <body>
         <Head />
         {children}
