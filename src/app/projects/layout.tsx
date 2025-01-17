7044c4aab69ffb307ed6e902bdd5c215b84438c4
haochend413 <dinghaochen413@gmail.com>
2025-01-16 19:41:02 -0600

    feat: aboutpage blink styling

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Ubuntu } from "next/font/google";
-
-const ubuntu = Ubuntu({
-  weight: ["300", "700"],
-  subsets: ["latin"],
-});
 
 export default function ProjectLayout({
   children,
@@ -21,9 +15,5 @@ export default function ProjectLayout({
   }
 
   //This returns the layout for the specific project pages. Configured.
-  return (
-    <html lang="en" className={ubuntu.className}>
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
